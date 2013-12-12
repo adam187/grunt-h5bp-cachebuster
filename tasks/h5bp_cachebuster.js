c6326ec1aa551af7dc90bbf78ebd83bff2f3af6c
Adam Misiorny <adam.misiorny@gmail.com>
2013-12-12 21:22:16 +0100

    add task file

---
+++ tasks/h5bp_cachebuster.js
@@ -0,0 +1,12 @@
+/*
+ * grunt-h5bp-cachebuster
+ * https://github.com/adam187/grunt-h5bp-cachebuster
+ *
+ * Copyright (c) 2013 Adam Misiorny
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+}
