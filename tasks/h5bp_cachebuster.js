4d6b9f60f37aca1bcbe4cc98b699dc2cc32f19a3
Adam Misiorny <adam.misiorny@gmail.com>
2013-12-14 08:54:48 +0100

    add checkig if file is local

--- tasks/h5bp_cachebuster.js
+++ tasks/h5bp_cachebuster.js
@@ -11,7 +11,7 @@
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('h5bp_cachebuster', 'add assets timestamp in css files', function () {
-    var crc, options, findUrls, getBasePath, urlParse, generateChecksum, cssBust;
+    var crc, options, findUrls, getBasePath, urlParse, isLocalFile, generateChecksum, cssBust;
 
     crc = require('crc');
 
@@ -28,8 +28,12 @@ module.exports = function(grunt) {
     };
 
     urlParse = function(url) {
-      var m = url.match(/(url\( *['"]?)(([A-Za-z0-9_\-\.\/]+)(\.(jpe?g|png|gif|svg)))(['"]? *\))/i);
+      var m = url.match(/(url\( *['"]?)(([A-Za-z0-9_\-\.\/:]+)(\.(jpe?g|png|gif|svg)))(['"]? *\))/i);
       return m ? {url: m[0], prefix: m[1], file: m[2], base: m[3], ext: m[5], sufix: m[6]} : null;
+    };
+
+    isLocalFile = function(filePath) {
+      return filePath.match(/^((https?|ftp):)?\/\//) ? false : true;
     };
 
     getBasePath = function(filePath) {
@@ -48,14 +52,15 @@ module.exports = function(grunt) {
           var parsedUrl, filePath, checksum, replacement;
 
           parsedUrl = urlParse(url);
-          filePath = basePath + parsedUrl.file;
-
-          if (grunt.file.exists(filePath)) {
-            checksum = generateChecksum(filePath, options.algorithm);
-            replacement = parsedUrl.prefix + parsedUrl.base + '.' + checksum + '.' + parsedUrl.ext + parsedUrl.sufix;
-            source = source.replace(parsedUrl.url, replacement);
-          } else {
-            grunt.log.warn('Asset file "' + filePath + '" not found.');
+          if (isLocalFile(parsedUrl.file)) {
+            filePath = basePath + parsedUrl.file;
+            if (grunt.file.exists(filePath)) {
+              checksum = generateChecksum(filePath, options.algorithm);
+              replacement = parsedUrl.prefix + parsedUrl.base + '.' + checksum + '.' + parsedUrl.ext + parsedUrl.sufix;
+              source = source.replace(parsedUrl.url, replacement);
+            } else {
+              grunt.log.warn('Asset file "' + filePath + '" not found.');
+            }
           }
 
         });
